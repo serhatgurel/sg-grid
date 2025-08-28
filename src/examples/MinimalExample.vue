ac7ac7c8e51702f4d2875b472e16334ccdccfcdc
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-08-29 00:27:16 +1000

    refactor: update SgColumn and SgGrid components for improved data handling and rendering

--- src/examples/MinimalExample.vue
+++ src/examples/MinimalExample.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SgGrid from '../components/SgGrid.vue'
 import SgColumn from '../components/SgColumn.vue'
-import SgTable from '../components/SgTable.vue'
+//import SgTable from '../components/SgTable.vue'
 import { computed } from 'vue'
 import rowsJson from './rows.json'
 
@@ -21,41 +21,43 @@ interface RowDef {
 
 const rows = computed(() => rowsJson as Array<Partial<RowDef>>)
 
-const columns = [
-  { key: 'title', field: 'title', label: 'Title', width: '200px' },
-  { key: 'firstName', field: 'firstName', label: 'First Name', width: '200px' },
-  { key: 'lastName', field: 'lastName', label: 'Last Name', width: '200px' },
-  { key: 'age', field: 'age', label: 'Age', width: '60px' },
-  { key: 'birthdate', field: 'birthdate', label: 'Birthdate', width: '120px' },
-  { key: 'gender', field: 'gender', label: 'Gender', width: '120px' },
-  { key: 'phone', field: 'phone', label: 'Phone', width: '120px' },
-  { key: 'email', field: 'email', label: 'Email', width: '200px' },
-  { key: 'salary', field: 'salary', label: 'Salary', width: '120px' },
-  { key: 'job', field: 'job', label: 'Job', width: '200px' },
-]
+// const columns = [
+//   { key: 'title', field: 'title', label: 'Title', width: '200px' },
+//   { key: 'firstName', field: 'firstName', label: 'First Name', width: '200px' },
+//   { key: 'lastName', field: 'lastName', label: 'Last Name', width: '200px' },
+//   { key: 'age', field: 'age', label: 'Age', width: '60px' },
+//   { key: 'birthdate', field: 'birthdate', label: 'Birthdate', width: '120px' },
+//   { key: 'gender', field: 'gender', label: 'Gender', width: '120px' },
+//   { key: 'phone', field: 'phone', label: 'Phone', width: '120px' },
+//   { key: 'email', field: 'email', label: 'Email', width: '200px' },
+//   { key: 'salary', field: 'salary', label: 'Salary', width: '120px' },
+//   { key: 'job', field: 'job', label: 'Job', width: '200px' },
+// ]
 </script>
 
 <template>
-  <!-- 
+  <!--
    <h3>Just a column</h3>
-  <SgColumn>{{ rows[2].title }} {{ rows[2].firstName }} {{ rows[2].lastName }}</SgColumn> 
+  <SgColumn>{{ rows[2].title }} {{ rows[2].firstName }} {{ rows[2].lastName }}</SgColumn>
   -->
   <hr />
 
-  <SgGrid :columns="columns" :rows="rows.slice(0, 3)" rowKey="id" caption="DECLARATIVE EXAMPLE">
+  <SgGrid :rows="rows" rowKey="id" caption="DECLARATIVE EXAMPLE">
     <SgColumn data-field="id" label="Id" />
     <SgColumn data-field="firstName" label="First Name" />
     <SgColumn data-field="lastName" label="Last Name" />
     <SgColumn data-field="email" label="Email" />
+    <SgColumn data-field="job" label="Job" />
+    <SgColumn data-field="salary" label="Salary" />
   </SgGrid>
-
+<!--
   <hr />
 
-  <SgGrid :columns="columns" :rows="rows.slice(0, 8)" rowKey="id"  caption="PROPS BASED EXAMPLE" />
+  <SgGrid :columns="columns" :rows="rows.slice(0, 8)" rowKey="id" caption="PROPS BASED EXAMPLE" />
 
   <hr />
 
   <SgTable :fields="columns" :items="rows.slice(20, 25)" caption="TABLE SLOTS EXAMPLE"></SgTable>
 
-  <hr />
+  <hr /> -->
 </template>
