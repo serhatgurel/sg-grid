a85b1aff80c608f552921f4785ae65c6e61e69df
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-08-29 23:13:49 +1000

    feat: update MinimalExample to use SgDeclarativeGrid and SgDeclarativeColumn components

--- src/examples/MinimalExample.vue
+++ src/examples/MinimalExample.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import SgGrid from '../components/SgGrid.vue'
 import SgColumn from '../components/SgColumn.vue'
-import SgTable from '../components/SgTable.vue'
 import { computed } from 'vue'
 import rowsJson from './rows.json'
+import SgDeclarativeGrid from '@/components/SgDeclarativeGrid.vue'
+import SgDeclarativeColumn from '@/components/SgDeclarativeColumn.vue'
 
 interface RowDef {
   id?: string
@@ -36,26 +37,33 @@ const columns = [
 </script>
 
 <template>
-  <!-- 
-   <h3>Just a column</h3>
-  <SgColumn>{{ rows[2].title }} {{ rows[2].firstName }} {{ rows[2].lastName }}</SgColumn> 
+  <!--
+    <h3>Just a column</h3>
+    <SgColumn>{{ rows[2].title }} {{ rows[2].firstName }} {{ rows[2].lastName }}</SgColumn>
   -->
-  <hr />
 
-  <SgGrid :columns="columns" :rows="rows.slice(0, 3)" rowKey="id" caption="DECLARATIVE EXAMPLE">
+  <SgDeclarativeGrid :rows="rows.slice(0, 5)" rowKey="id" caption="DECLARATIVE EXAMPLE">
+    <SgDeclarativeColumn data-field="id" label="Id" />
+    <SgDeclarativeColumn data-field="title" label="Title" />
+    <SgDeclarativeColumn data-field="firstName" label="First Name" />
+    <SgDeclarativeColumn data-field="lastName" label="Last Name" />
+    <SgDeclarativeColumn data-field="email" label="Email" />
+    <SgDeclarativeColumn data-field="phone" label="Phone" />
+  </SgDeclarativeGrid>
+
+  <SgGrid :rows="rows.slice(0, 3)" rowKey="id" caption="* DECLARATIVE EXAMPLE NO LABEL *">
+    <SgColumn data-field="id" />
+    <SgColumn data-field="firstName"  />
+    <SgColumn data-field="lastName"  />
+    <SgColumn data-field="email"  />
+  </SgGrid>
+
+  <SgGrid :rows="rows.slice(0, 3)" rowKey="id" caption="* DECLARATIVE EXAMPLE WITH LABEL *">
     <SgColumn data-field="id" label="Id" />
     <SgColumn data-field="firstName" label="First Name" />
     <SgColumn data-field="lastName" label="Last Name" />
     <SgColumn data-field="email" label="Email" />
   </SgGrid>
 
-  <hr />
-
-  <SgGrid :columns="columns" :rows="rows.slice(0, 8)" rowKey="id"  caption="PROPS BASED EXAMPLE" />
-
-  <hr />
-
-  <SgTable :fields="columns" :items="rows.slice(20, 25)" caption="TABLE SLOTS EXAMPLE"></SgTable>
-
-  <hr />
+  <SgGrid :columns="columns" :rows="rows.slice(0, 8)" rowKey="id" caption="PROPS BASED EXAMPLE" />
 </template>
