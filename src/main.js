33a4606e145eb0e93e68892df028c8a2d3d644df
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-08-29 23:33:36 +1000

    refactor: remove main.js file as part of project restructuring

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vue_1 = require("vue");
-var App_vue_1 = require("./App.vue");
-require("./style.css");
-(0, vue_1.createApp)(App_vue_1.default).mount('#app');
