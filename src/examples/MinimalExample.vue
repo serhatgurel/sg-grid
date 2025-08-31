46c0bb140163f0104da5612c9281f72a7b204eae
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-08-31 13:20:47 +1000

    fix: update import path for people data in MinimalExample.vue

--- src/examples/MinimalExample.vue
+++ src/examples/MinimalExample.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import rowsJson from './rows.json'
+import rowsJson from './people.json'
 import SgGrid from '@/components/SgGrid.vue'
 import SgColumn from '@/components/SgColumn.vue'
 
