176e692a49f189a046f45e5add107da848938095
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-08-31 14:47:56 +1000

    fix: adjust column widths for improved layout in MinimalExample.vue

--- src/examples/MinimalExample.vue
+++ src/examples/MinimalExample.vue
@@ -24,7 +24,7 @@ const rows = computed(() => rowsJson as unknown as Array<Partial<RowDef>>)
 
 const columns = [
   { key: 'title', field: 'title', caption: 'Title', width: '200px' },
-  { key: 'firstName', field: 'firstName', caption: 'First Name', width: '200px' },
+  { key: 'firstName', field: 'firstName', caption: 'First Name', width: '100px' },
   { key: 'lastName', field: 'lastName', caption: 'Last Name', width: '200px' },
   { key: 'age', field: 'age', caption: 'Age', width: '60px' },
   { key: 'birthdate', field: 'birthdate', caption: 'Birthdate', width: '120px' },
@@ -33,7 +33,12 @@ const columns = [
   { key: 'email', field: 'email', caption: 'Email', width: '200px' },
   { key: 'salary', field: 'salary', caption: 'Salary', width: '120px' },
   { key: 'job', field: 'job', caption: 'Job', width: '200px' },
-  { key: 'exclamation', field: (row: Partial<RowDef>) => (row.firstName ? row.firstName + '!' : ''), caption: 'Exclamation', width: '120px' },
+  {
+    key: 'exclamation',
+    field: (row: Partial<RowDef>) => (row.firstName ? row.firstName + '!' : ''),
+    caption: 'Exclamation',
+    width: '120px',
+  },
 ]
 </script>
 
@@ -44,32 +49,36 @@ const columns = [
   -->
 
   <SgGrid :rows="rows.slice(0, 5)" rowKey="id" caption="DECLARATIVE EXAMPLE">
-    <SgColumn data-field="id" caption="Id" />
-    <SgColumn data-field="title" caption="Title" />
-    <SgColumn data-field="firstName" caption="First Name" />
-    <SgColumn data-field="lastName" caption="Last Name" />
-    <SgColumn data-field="email" caption="Email" />
-    <SgColumn data-field="phone[0].type" caption="Phone" />
+    <SgColumn data-field="id" caption="Id" width="60px" />
+    <SgColumn data-field="title" caption="Title" width="10px" />
+    <SgColumn data-field="firstName" caption="First Name" width="100px" />
+    <SgColumn data-field="lastName" caption="Last Name" width="200px" />
+    <SgColumn data-field="email" caption="Email" width="200px" />
+    <SgColumn data-field="phone[0].type" caption="Phone" width="120px" />
   </SgGrid>
 
   <SgGrid :rows="rows.slice(0, 3)" rowKey="id" caption="* DECLARATIVE EXAMPLE NO CAPTION *">
-    <SgColumn data-field="id" />
-    <SgColumn data-field="firstName" />
-    <SgColumn data-field="lastName" />
-    <SgColumn data-field="email" />
-    <SgColumn data-field="job" />
+    <SgColumn data-field="id" width="60px" />
+    <SgColumn data-field="firstName" width="100px" />
+    <SgColumn data-field="lastName" width="200px" />
+    <SgColumn data-field="email" width="200px" />
+    <SgColumn data-field="job" width="200px" />
   </SgGrid>
 
   <SgGrid :rows="rows.slice(0, 3)" rowKey="id" caption="* DECLARATIVE EXAMPLE WITH CAPTION *">
-    <SgColumn data-field="id" caption="Id" />
-    <SgColumn data-field="firstName" caption="First Name" />
-    <SgColumn data-field="lastName" caption="Last Name" />
-    <SgColumn data-field="email" caption="Email" />
-    <SgColumn data-field="job" caption="Job" />
-    <SgColumn data-field="salary" caption="Salary" />
-    <SgColumn data-field="phone[0].number" caption="Phone" />
-    <SgColumn data-field="address[0].country.code" caption="Country" />
-    <SgColumn :data-field="(row) => (row.firstName ? row.firstName + '@' : '')" caption="Exclamation" />
+    <SgColumn data-field="id" caption="Id" width="60px" />
+    <SgColumn data-field="firstName" caption="First Name" width="100px" />
+    <SgColumn data-field="lastName" caption="Last Name" width="200px" />
+    <SgColumn data-field="email" caption="Email" width="200px" />
+    <SgColumn data-field="job" caption="Job" width="200px" />
+    <SgColumn data-field="salary" caption="Salary" width="120px" />
+    <SgColumn data-field="phone[0].number" caption="Phone" width="120px" />
+    <SgColumn data-field="address[0].country.code" caption="Country" width="120px" />
+    <SgColumn
+      :data-field="(row) => (row.firstName ? row.firstName + '@' : '')"
+      caption="Exclamation"
+      width="120px"
+    />
   </SgGrid>
 
   <SgGrid :columns="columns" :rows="rows.slice(0, 8)" rowKey="id" caption="PROPS BASED EXAMPLE" />
