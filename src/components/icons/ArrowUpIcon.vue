a5dcf4a19b7c8291925547518f47a397222e4b62
Serhat Gurel <serhat.gurel@outlook.com.au>
2025-09-07 01:27:26 +1000

    feat: add new SVG icons for ArrowDown, ArrowUp, Filter, and Sort

---
+++ src/components/icons/ArrowUpIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    viewBox="0 -960 960 960"
+    xmlns="http://www.w3.org/2000/svg"
+    :fill="color"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ size?: string | number; color?: string }>()
+const size = props.size ?? '16'
+const color = props.color ?? 'currentColor'
+</script>
---
+++ src/components/icons/SortIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    viewBox="0 -960 960 960"
+    xmlns="http://www.w3.org/2000/svg"
+    :fill="color"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ size?: string | number; color?: string }>()
+const size = props.size ?? '16'
+const color = props.color ?? 'currentColor'
+</script>
